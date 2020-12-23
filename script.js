357bcfd799e11b0f7e63be06959cbdb670318262
Sammy Chen <chensongsen03@gmail.com>
2020-12-23 15:11:42 -0600

    Scalable to squares per side

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const container = document.querySelector(`#container`);
+let squares = 16;
 function makeGrid(){ 
-for(let i = 0; i< (16*16); i++){
+for(let i = 0; i< (squares*squares); i++){
     const div = document.createElement(`div`);
     div.classList.add("cell");
+    container.setAttribute('style', `grid-template-columns : repeat(${squares}, 1em); grid-template-rows : repeat(${squares}, 1em)`);
+    div.addEventListener('mouseover', function(e){
+        e.target.style.backgroundColor = "black";
+    });
     container.appendChild(div);
     }
 }
 makeGrid();
+const button = document.querySelector(`#btn`);
+button.addEventListener('click', () => {
+    let question = parseInt(prompt("How many squares per side? "));
+    squares = question;
+    while(container.firstElementChild){
+        container.removeChild(container.lastElementChild);
+    }
+    makeGrid();
+});
