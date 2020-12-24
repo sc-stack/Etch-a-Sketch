f56782c982523f3999ca5a1a22ef0bb9b67871fc
Sammy Chen <chensongsen03@gmail.com>
2020-12-23 21:20:32 -0600

    changed grid to have fixed size

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function makeGrid(){
 for(let i = 0; i< (squares*squares); i++){
     const div = document.createElement(`div`);
     div.classList.add("cell");
-    container.setAttribute('style', `grid-template-columns : repeat(${squares}, 1em); grid-template-rows : repeat(${squares}, 1em)`);
+    container.setAttribute('style', `grid-template-columns : repeat(${squares}, 1fr); grid-template-rows : repeat(${squares}, 1fr)`);
     div.addEventListener('mouseover', function(e){
         e.target.style.backgroundColor = "black";
     });
@@ -14,7 +14,7 @@ for(let i = 0; i< (squares*squares); i++){
 makeGrid();
 const button = document.querySelector(`#btn`);
 button.addEventListener('click', () => {
-    let question = parseInt(prompt("How many squares per side? "));
+    let question = parseInt(prompt("How many squares per side?"));
     squares = question;
     while(container.firstElementChild){
         container.removeChild(container.lastElementChild);
